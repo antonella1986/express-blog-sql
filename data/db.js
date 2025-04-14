e8c5df1b349a9b2f97c13d71fdb5bf7698b9a94b
antonella1986 <antonella.leo.mail@gmail.com>
2025-04-14 13:02:16 +0200

    add gitignore file, edit dev mode in package.json and add db.js to configure and connect db

---
+++ data/db.js
@@ -0,0 +1,25 @@
+const mysql = require('mysql2');
+
+const credentials = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
+}
+
+console.log({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
+});
+
+const connection = mysql.createConnection(credentials);
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to database');
+})
+
+module.exports = connection;
